911f990eee6e334b6fedfa04dbd586b8232bafb2
Dhiraj Shiwankar <dhirajshivankar90@gmail.com>
2026-01-08 11:55:33 +0530

    Create Trivia_bot.js
    Variable and String Problem.

---
+++ Trivia_bot.js
@@ -0,0 +1,36 @@
+// ********************************** Question ************************************************
+// You should log "Hello! I'm your coding fun fact guide!" to the console as a greeting message to the user.
+// You should create three variables: botName, botLocation, and favoriteLanguage, that store the bot's name, where it's from, and its favorite coding language, respectively.
+// You should log "My name is (botName) and I live on (botLocation)." to the console.
+// You should log "My favorite programming language is (favoriteLanguage)." to the console.
+// You should use let to create a codingFact variable and assign it a string that is a fun fact about your bot's favorite coding language and include the use of the favoriteLanguage variable.
+// You should log the codingFact to the console.
+// You should reassign the codingFact variable to a new fact about the bot's favorite language using the favoriteLanguage variable again.
+// You should log the codingFact to the console again.
+// You should reassign the codingFact variable again to another new fact about the bot's favorite language using the favoriteLanguage variable.
+// You should log the codingFact to the console a third time.
+// You should log "It was fun sharing these facts with you. Goodbye! - (botName) from (botLocation)." to the console as a farewell statement from the bot.
+
+
+// ********************************** Answer ************************************************
+
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "Ronin";
+const botLocation = "Nagpur";
+const favoriteLanguage = "HTML, CSS, JS";
+
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`)
+
+let codingFact = `${favoriteLanguage} is complete in 30 days.`;
+console.log(codingFact)
+
+codingFact = `${favoriteLanguage} it very easy language.`;
+console.log(codingFact);
+
+codingFact = `${favoriteLanguage} is one of the most popular language for web development .`;
+console.log(codingFact);
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);
+
