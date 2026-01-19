07964e190abe37b2ae29eb5377b6e116bc18d044
Dhiraj Shiwankar <dhirajshivankar90@gmail.com>
2026-01-19 19:20:16 +0530

    Create a function named mutation.

---
+++ named_mutation.js
@@ -0,0 +1,23 @@
+////////////////////////////////////// QUESTION ///////////////////////////////////
+// Create a function named mutation that takes an array as its argument.
+// mutation should return true if the string in the first element of the array contains all of the letters of the string in the second element of the array, and false otherwise. For example:
+// mutation(["hello", "Hello"]), should return true because all of the letters in the second string are present in the first, ignoring case.
+// mutation(["hello", "hey"]) should return false because the string hello does not contain a y.
+// mutation(["Alien", "line"]), should return true because all of the letters in line are present in Alien.
+
+////////////////////////////////////// Answer ///////////////////////////////////
+
+function mutation (arr) {
+  let firstarr = arr[0].toLowerCase();
+  let secondarr = arr[1].toLowerCase();
+
+  for (let char of secondarr) {
+    if (!firstarr.includes(char)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+let arr1 = mutation(["hello", "Hello"]);
+console.log(arr1);
