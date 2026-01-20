8bb993d8a81cfbc302e204c58fda7dfcd118bf4b
Dhiraj Shiwankar <dhirajshivankar90@gmail.com>
2026-01-20 07:46:44 +0530

    Create algorithm.js

---
+++ algorithm.js
@@ -0,0 +1,18 @@
+// Implement the Chunky Monkey Algorithm
+// Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
+
+// User Stories:
+
+// Write a function named chunkArrayInGroups that takes an array as first argument and a number as second argument. The function should split the array into smaller arrays of length equal to the second argument and returns them as a two-dimensional array.
+
+function chunkArrayInGroups(arr, size) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i += size) {
+    result.push(arr.slice(i, i + size));
+  }
+
+  return result;
+}
+
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2));
