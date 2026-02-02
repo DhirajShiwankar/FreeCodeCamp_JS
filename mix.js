482839b013f59bd4d364e769c7d57254565bcbd5
Dhiraj Shiwankar <dhirajshivankar90@gmail.com>
2026-02-02 23:32:10 +0530

    Create mix.js

---
+++ mix.js
@@ -0,0 +1,15 @@
+export function timeToMixJuice(juice) {
+  switch (juice) {
+    case 'Pure Strawberry Joy':
+      return 0.5;
+    case 'Energizer':
+    case 'Green Garden':
+      return 1.5;
+    case 'Tropical Island':
+      return 3;
+    case 'All or Nothing':
+      return 5;
+    default:
+      return 2.5;
+  }
+}
