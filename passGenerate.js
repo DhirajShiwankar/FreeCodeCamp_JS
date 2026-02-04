5c63f93917c78d86bfa7f46f732da65218af2947
Dhiraj Shiwankar <dhirajshivankar90@gmail.com>
2026-02-04 15:43:27 +0530

    Create random password generator

---
+++ passGenerate.js
@@ -0,0 +1,13 @@
+function generatePassword (length) {
+  let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+  let password = "";
+
+  for (let i = 1; i <= length; i++) {
+    let randomIndex = Math.floor(Math.random() * chars.length);
+    password += chars[randomIndex];
+  }
+
+  return password;
+}
+
+console.log(generatePassword(12));
