3d89ab002dc3df367be6ff3e7f6c9912cebaaa5d
Dhiraj Shiwankar <dhirajshivankar90@gmail.com>
2026-02-09 22:37:06 +0530

    Create between_leap_year.js

---
+++ between_leap_year.js
@@ -0,0 +1,9 @@
+function findLeapYears(start, end) {
+  for (let year = start; year <= end; year++) {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+      console.log(year);
+    }
+  }
+}
+
+findLeapYears(2000, 2025);
